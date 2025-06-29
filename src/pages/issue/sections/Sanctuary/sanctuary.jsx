c034ca42fcdf6a76975c27b3cd5a78f15788b4bd
Masha <90609398+Mashha@users.noreply.github.com>
2025-06-29 16:35:20 +0200

    added video section and updated json

--- src/pages/issue/sections/Sanctuary/sanctuary.jsx
+++ src/pages/issue/sections/Sanctuary/sanctuary.jsx
@@ -1,4 +1,4 @@
-import "./sanctuary.css"
+import "./sanctuary.css";
 
 const FarmAnimalStoriesSection = ({ data }) => {
   return (
@@ -6,19 +6,30 @@ const FarmAnimalStoriesSection = ({ data }) => {
       <div className='rescue-container'>
         <div className='rescue-content'>
           <h2 className='section-title'>{data.AnimalStories.title}</h2>
-          <p className='section-subtitle'></p>
         </div>
         <div className='video-wrapper'>
           <iframe
             width='100%'
             height='500'
-            src='https://www.youtube.com/embed/fp5KJQ_xakU?si=CrKYrnAt6MzCBm_o'
-            title='Farm Animal Stories with Happy Endings'
+            src={data.AnimalStories.link}
+            title={data.AnimalStories.title}
             allow='web-share'
             referrerPolicy='strict-origin-when-cross-origin'
             allowFullScreen
           />
         </div>
+        {data.AnimalStories.sanctuary_link && (
+          <div className='sanctuary-support'>
+            <a
+              href={data.AnimalStories.sanctuary_link}
+              target='_blank'
+              rel='noopener noreferrer'
+              className='sanctuary-link'
+            >
+              💚 {data.AnimalStories.support} {data.AnimalStories.sanctuary_name}
+            </a>
+          </div>
+        )}
       </div>
     </section>
   );
