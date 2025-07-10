7f9a050fbd32a6df6b2a3f750c4ddf3f4fa8807d
Masha <90609398+Mashha@users.noreply.github.com>
2025-07-10 23:16:44 +0200

    changed image section, translations

--- src/pages/issue/sections/Images/Images.jsx
+++ src/pages/issue/sections/Images/Images.jsx
@@ -1,46 +1,37 @@
-import './images.css';
+import "./images.css";
+import AnimatedImage from "../../../../components/image/AnimatedImage";
+import { useTranslation } from "react-i18next";
 
 const ImagesSection = () => {
+  const { t } = useTranslation();
   const images = [
-    'https://picsum.photos/400/300?random=1',
-    'https://picsum.photos/400/300?random=2',
-    'https://picsum.photos/400/300?random=3',
-    'https://picsum.photos/400/300?random=4',
-    'https://picsum.photos/400/300?random=5',
-    'https://picsum.photos/400/300?random=6',
-    'https://picsum.photos/400/300?random=7',
-    'https://picsum.photos/400/300?random=8'
+    "https://picsum.photos/400/300?random=1",
+    "https://picsum.photos/400/300?random=2",
+    "https://picsum.photos/400/300?random=3",
+    "https://picsum.photos/400/300?random=4",
+    "https://picsum.photos/400/300?random=5",
+    "https://picsum.photos/400/300?random=6",
+    "https://picsum.photos/400/300?random=7",
+    "https://picsum.photos/400/300?random=8",
   ];
 
   return (
-    <section className="hidden-section">
-      <div className="hidden-section__container">
-        <h2 className="hidden-section__title">
-          What They Don't Want You to See
+    <section className='hidden-section'>
+      <div className='hidden-section-container'>
+        <h2 className='hidden-section-title'>
+          {t("shared.hiddenTruthTitle")}
         </h2>
-        
-        <div className="hidden-section__scroll-wrapper">
-          <div className="hidden-section__scroll-container">
-            {/* First set of images */}
-            {images.map((img, index) => (
-              <div key={`first-${index}`} className="hidden-section__image-card">
-                <img 
-                  src={img} 
-                  alt={`Hidden image ${index + 1}`}
-                  className="hidden-section__image"
-                />
-              </div>
-            ))}
-            
-            {/* Duplicate set for seamless loop */}
-            {images.map((img, index) => (
-              <div key={`second-${index}`} className="hidden-section__image-card">
-                <img 
-                  src={img} 
-                  alt={`Hidden image ${index + 1} duplicate`}
-                  className="hidden-section__image"
-                />
-              </div>
+
+        <div className='hidden-section__scroll-wrapper'>
+          <div className='images-grid'>
+            {images.map((image, index) => (
+              <AnimatedImage
+                key={index}
+                src={image}
+                alt={`Hidden image ${index + 1}`}
+                animName={index % 2 === 0 ? "slideFromLeft" : "slideFromRight"}
+                className='grid-image'
+              />
             ))}
           </div>
         </div>
