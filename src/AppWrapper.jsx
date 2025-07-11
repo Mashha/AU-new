453a0592828580a2aa8d969fae78871048402829
Žan Pukmajster <45610204+pukmajster@users.noreply.github.com>
2025-07-11 23:22:53 +0200

    force splash screen to always show

--- src/AppWrapper.jsx
+++ src/AppWrapper.jsx
@@ -3,27 +3,20 @@ import App from "./App";
 import Loader from "./components/loader/Loader";
 
 const AppWrapper = () => {
-  const [showLoader, setShowLoader] = useState(false);
+  const [showLoader, setShowLoader] = useState(true);
 
   useEffect(() => {
-    const alreadyVisited = sessionStorage.getItem("visited");
-
-    const isFirstVisit = !alreadyVisited;
-    const fromExternal =
-      document.referrer === "" ||
-      !document.referrer.includes(window.location.hostname);
-
-    if (isFirstVisit && fromExternal) {
-      sessionStorage.setItem("visited", "true");
-      setShowLoader(true);
-
-      setTimeout(() => {
-        setShowLoader(false);
-      }, 4000);
-    }
+    setTimeout(() => {
+      setShowLoader(false);
+    }, 4000);
   }, []);
 
-  return showLoader ? <Loader /> : <App />;
+  return (
+    <>
+      {showLoader ? <Loader /> : null}
+      <App />;
+    </>
+  );
 };
 
 export default AppWrapper;
